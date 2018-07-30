7b87d965cf4abfd59cce8506120d4290d586bb89
IBM343-PC0D6U7L\nikunj <nikunj.agarwal@metlife.com>
2018-07-30 09:04:09 +0530

    first commit

---
+++ test/create_test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Creating Records',()=>{
+    it('dummy',()=>{
+        assert(1+1 ===2);
+    });
+
+    it('saves a user',(done)=>{
+        const joe = new User({ name:'Joe' });
+        joe.save()
+        .then(()=>{
+            // assert(joe.isNew===false);
+            assert(!joe.isNew);
+            done();
+        })
+        .catch(()=>{
+
+        });
+    });
+});
